0589ea363e4a88cdd7df03eacef5455cb288ba7a
logesh <logeshe9159@gmail.com>
2024-02-22 22:19:05 +0530

    done only for 1-digit cal

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bt1.setOnClickListener{
+            binding.inputt.append("1")
+        }
+        binding.bt2.setOnClickListener{
+            binding.inputt.append("2")
+        }
+        binding.bt3.setOnClickListener{
+            binding.inputt.append("3")
+        }
+        binding.bt4.setOnClickListener{
+            binding.inputt.append("4")
+        }
+        binding.bt5.setOnClickListener{
+            binding.inputt.append("5")
+        }
+        binding.bt6.setOnClickListener{
+            binding.inputt.append("6")
+        }
+        binding.bt7.setOnClickListener{
+            binding.inputt.append("7")
+        }
+        binding.bt8.setOnClickListener{
+            binding.inputt.append("8")
+        }
+        binding.bt9.setOnClickListener{
+            binding.inputt.append("9")
+        }
+        binding.bt0.setOnClickListener{
+            binding.inputt.append("0")
+        }
+        binding.btDiv.setOnClickListener{
+            binding.inputt.append("/")
+        }
+        binding.btMul.setOnClickListener{
+            binding.inputt.append("X")
+        }
+        binding.btSub.setOnClickListener{
+            binding.inputt.append("-")
+        }
+        binding.btAdd.setOnClickListener{
+            binding.inputt.append("+")
+        }
+        binding.btClear.setOnClickListener{
+            binding.inputt.setText("")
+        }
+        binding.btEql.setOnClickListener{
+            val input: String = binding.inputt.getText().toString()
+            val one: Int = Integer.parseInt(input.slice(listOf(0)))
+//            binding.inputt.setText(one.toString())
+            val two: String = input.slice(listOf(1))
+            val three: Int = Integer.parseInt(input.slice(listOf(2)))
+            if (two=="+"){
+                binding.inputt.setText((one+three).toString())
+            }
+            if (two=="-"){
+                binding.inputt.setText((one-three).toString())
+            }
+            if (two=="X"){
+                binding.inputt.setText((one*three).toString())
+            }
+            if (two=="/"){
+                binding.inputt.setText((one/three).toString())
+            }
+        }
+
     }
 }
